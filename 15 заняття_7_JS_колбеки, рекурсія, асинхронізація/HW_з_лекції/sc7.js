ae6ae4b9723b2f78a7549f7997e1990e815fb7ff
bessmertnyi <bosyitaras@gmail.com>
2019-09-03 19:47:32 +0300

    add new task

---
+++ 15 заняття_7_JS_колбеки, рекурсія, асинхронізація/HW_з_лекції/sc7.js
@@ -0,0 +1,57 @@
+
+
+
+// let i = 0;
+// let xxx = [];
+//
+// function disband () {
+//
+//     if (Array.isArray(arr[i])) {
+//
+//             return xxx.flat(0);
+//
+//         // if (i < arr.length) {
+//         //
+//         //     console.log(arr[i]);
+//         //     i++;
+//
+//
+//         disband();
+//
+//     }
+//
+// }
+//
+// disband();
+// console.log(xxx);
+
+/*
+function flattenArray(arr) {
+
+    let newArr = [];
+
+    function flatten(arr, newArr) {
+
+        for (i = 0; i < arr.length; i++) {
+
+            if (Array.isArray(arr[i]) === false) {
+                newArr.push(arr[i]);
+            }
+
+            else {
+                flatten(arr[i], newArr);
+            }
+        }
+    }
+
+    flatten(arr, newArr);
+
+    console.log(newArr);
+}
+
+flattenArray([1,2,3,4,5,[10,20,30,[100,200,300],40,50],6,7,8]);*/
+
+let arr = [1,2,3,4,5,[10,20,30,[100,200,300],40,50],6,7,8];
+
+
+
