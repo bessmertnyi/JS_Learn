1fa389697bdbca35e8d82a4d06c7f1719b4bdb3e
bessmertnyi <bosyitaras@gmail.com>
2019-09-08 15:23:21 +0300

    add new task

--- 16 заняття_8_JS_ООП/практичне/sc8.2.js
+++ 16 заняття_8_JS_ООП/практичне/sc8.2.js
@@ -14,7 +14,7 @@ let Solya = new Popelushka('Solya', 35, 37);
 let Natalja = new Popelushka('Natalja', 25, 35);
 let Lilja = new Popelushka('Lilja', 29, 39);
 
-let arrPopelushka = [Vika, Oksana, Solya, Natalja, Lilja];
+
 
 class Princ {
     constructor(name, age, tuflia){
@@ -30,29 +30,41 @@ let Dimas = new Princ('Dimas', 24, 37);
 let Victor = new Princ('Victor', 23, 38);
 let Nazar = new Princ('Nazar', 32, 39);
 
+let arrPopelushka = [Vika, Oksana, Solya, Natalja, Lilja];
 let arrPrinc = [Roman, Taras, Dimas, Victor, Nazar];
+let mar = [];
 
 
-arrPopelushka.map(function (value) {
-    console.log(value.size, value.name);
-});
+/*function sutyner(a, b) {
 
+    for (i = 0; i < arrPopelushka.length; i++){
+        for (j = 0; j < arrPrinc.length; j++){
+            if (arrPopelushka[i].size === arrPrinc[j].tuflia) {
+                mar.push(arrPrinc[j].name, '+', arrPopelushka[i].name);
+            }
+        }
+    }
+}
 
-let xxx = arrPopelushka.map(function (value) {
-    return value.size
-});
-let yyy = arrPrinc.map(function (value) {
-    return value.tuflia
-});
-console.log(xxx);
-console.log(yyy);
-
-
-// === arrPrinc.map(value => value.tuflia) {
+sutyner(arrPopelushka, arrPrinc);
+console.log(mar);*/
 
 
 
-arrPrinc.forEach((xxx,yyy) => {
-    xxx.tuflia.findIndex(xxx.tuflia === yyy.size);
-    console.log(xxx.tuflia);
-});
+
+    arrPrinc.forEach(function (valuePrinc) {
+        arrPopelushka.findIndex(function (valuePopl) {
+            if (valuePrinc.tuflia === valuePopl.size){
+                console.log(valuePrinc.name, '+',  valuePopl.name);
+            }
+        })
+    });
+
+
+
+
+
+
+
+
+
