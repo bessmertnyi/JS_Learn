52d2d2bbbad39da1a7ec32dc0a06fe421c3a3d6f
bessmertnyi <bosyitaras@gmail.com>
2019-10-03 01:08:27 +0300

    add les2 - express

--- Node_JS/les_2_express/HW/app.js
+++ Node_JS/les_2_express/HW/app.js
@@ -5,10 +5,18 @@ const path = require('path'); //  бібліотека яка працює зі 
 const app = express();
 app.use(express.static(path.join(__dirname, 'static')));
 
+app.engine('.hbs', expHbs({
+    extname: '.hbs' // обробляє файли .хбс
+}));
+
+app.set('view engine', '.hbs'); // використовувати двіхок engine для файлів .хбс
+app.set('views', path.join(__dirname, 'static')); // шлях до всіх 'вюшок', де вони лежать
 
 app.get('/', (req, res) => {
-    res.end("OK");
-    console.log('OK 3000');
+    res.render('main');
+
+    // res.end("OK");
+    // console.log('OK 3000');
 
     // console.log(__dirname);  шлях до папки
     // console.log(__filename);
