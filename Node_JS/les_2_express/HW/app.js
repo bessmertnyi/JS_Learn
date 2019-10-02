866f168718ccf2345badadbc988314d213e0f9c0
bessmertnyi <bosyitaras@gmail.com>
2019-10-03 00:45:31 +0300

    add les2 - express

--- Node_JS/les_2_express/HW/app.js
+++ Node_JS/les_2_express/HW/app.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const expHbs = require('express-handlebars');
-const path = require('path');
+const path = require('path'); //  бібліотека яка працює зі шляхами (назвами папок)
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'static')));
+
 
 app.get('/', (req, res) => {
+    res.end("OK");
+    console.log('OK 3000');
 
-    console.log('H E L L O W');
+    // console.log(__dirname);  шлях до папки
+    // console.log(__filename);
+
 });
 
-app.listen(3000, ()=> {
+app.get('/login', (req, res) => {
+    res.end("OK");
+    console.log('OK 3000');
+});
+
+app.get('/register', (req, res) => {
+    res.end("OK");
+    console.log('OK 3000');
+});
+
+app.all('*', (req, res) => {
+    res.json('NOT PAGE, 404')
+});
+
+
+app.listen(3000, () => {
     console.log('H E L L O W');
 });
