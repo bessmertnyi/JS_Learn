8a14764d862a28934742bbbf863ac2becc04c31b
bessmertnyi <bosyitaras@gmail.com>
2019-10-08 00:09:57 +0300

    add les2 - express_2 (task ЛУН) + квартирки

---
+++ Node_JS/les_2/HW/app.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const expHbs = require('express-handlebars');
+const path = require('path'); //  бібліотека яка працює зі шляхами (назвами папок)
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); // розпізнає той JSON ))
+
+const users = [];
+const houses = [];
+
+app.engine('.hbs', expHbs({ // двіжок
+    extname: '.hbs', // обробляє файли .хбс
+    defaultLayout: null // фіксить папку Layout
+}));
+
+app.set('view engine', '.hbs'); // використовувати двіхок engine для файлів .хбс
+app.set('views', path.join(__dirname, 'static')); // шлях до всіх 'вюшок', де вони лежать
+
+app.get('/', (req, res) => {
+    res.render('main');
+    // console.log(__dirname);  // шлях до папки
+    // console.log(__filename);  // шлях до файлу
+});
+
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+
+app.get('/register', (req, res) => {
+    res.render('register')
+});
+
+app.get('/house', (req, res) => {
+    res.render('house')
+});
+
+
+app.post('/register', (req, res) => {
+    const user = req.body;
+    user.user_id = users.length + 1;
+    users.push(user);
+    console.log(user);
+});
+
+app.post('/house', (req, res) => {
+    const newHouse = req.body;
+    newHouse.house_id = houses.length + 1;
+    houses.push(newHouse);
+    console.log(newHouse);
+
+    // res.redirect(`/house/${newHouse.house_id}`)
+});
+
+app.get('/user/:id', (req, res) => {
+    const userSearch = users.find(ses =>
+        +req.params.id === ses.user_id
+    );
+    console.log(req.params);
+    res.json(userSearch)
+});
+
+app.get('/house/:id', (req, res) => {
+    const houseSearch = houses.find(oeo =>
+        +req.params.id === oeo.house_id
+    );
+    console.log(req.params);
+    res.json(houseSearch)
+});
+
+app.post('/login', (req, res) => {
+    const log = req.body;
+
+    const foundUser = users.find(search => search.userName === log.name && search.userPassword === log.password);
+    foundUser ? res.redirect(`/user/${foundUser.user_id}`) : res.status(404).render('NOT PAGE, 404')
+    /*users.forEach(search => {
+        if (search.userName === log.name && search.userPassword === log.password) {
+            res.redirect(`/user/${search.user_id}`)
+        } else {
+            res.json('NOT PAGE, 404')
+        }
+    });*/
+});
+
+app.post('/search', (req, res) => {
+    const info = req.body;
+
+    const foundHouse = houses.find(search => search.city === info.city);
+    foundHouse ? res.redirect(`/house/${foundHouse.house_id}`) : res.status(404).render('NOT PAGE, 404')
+
+});
+
+app.all('*', (req, res) => {
+    res.json('NOT PAGE, 404')
+});
+
+app.listen(3000, () => {
+    console.log('R U N  -  3 0 0 0');
+});
