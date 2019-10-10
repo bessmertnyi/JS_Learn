14f4f6f9260a8fda7f137f56079527b0a7794ab8
bessmertnyi <bosyitaras@gmail.com>
2019-10-10 20:24:05 +0300

    add les2 - express_2 (task ЛУН) + controllers + middleware + validators

--- Node_JS/les_2/controllers/user/createUser.js
+++ Node_JS/les_2/controllers/user/createUser.js
@@ -3,7 +3,8 @@ const { provider } = require('../../dataBase');
 module.exports = async  (req, res) => {
     const {name, email, password} = req.body;
     const query = `INSERT INTO user (name, email, password) VALUE (?, ?, ?)`;
+
     await provider.promise().query(query, [name, email, password]);
+
     res.render('register')
 };
-22
